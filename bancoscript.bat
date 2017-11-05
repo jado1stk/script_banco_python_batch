5841117004eee84207f6700581baf9e366f9bd98
jado1stk <r3snapest@gmail.com>
2017-11-05 19:00:25 -0200

    Script banco PYTHON BATCH

--- bancoscript.bat
+++ bancoscript.bat
@@ -1,4 +1,4 @@
 @echo off
-C:\python34\Scripts\pip install pymongo
-C:\python34\python.exe c:\python34\scriptbanco.py %*
+C:\python36\Scripts\pip install pymongo
+C:\python36\python.exe c:\python34\scriptbanco.py %*
 pause
